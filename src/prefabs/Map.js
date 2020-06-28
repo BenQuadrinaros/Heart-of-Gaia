f0941ee226e17e6d831344a866160a04934fe0c3
Ben Rowland <rowland.ben.j@gmail.com>
2020-06-27 22:08:12 -0700

    Maps and Tiles update

--- src/prefabs/Map.js
+++ src/prefabs/Map.js
@@ -1,10 +1,32 @@
 class Map extends Phaser.GameObjects.Sprite {
 
-    constructor(scene, x, y, texture, frame, name, grid) {
+    constructor(scene, x, y, texture, frame, grid, properties) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
 
+        this.name = grid[0] + " - " + properties[0];
+        this.grid = grid[1];
+        this.load.image("open", "./assets/"+properties[1]+".png");
+        this.load.image("closed", "./assets/"+properties[2]+".png");
+        this.load.image("exit", "./assets/"+properties[3]+".png");
+        this.load.image("return", "./assets/"+properties[4]+".png");
+        this.load.image("trap", "./assets/"+properties[5][0]+".png");
+        this.load.image("buff", "./assets/"+properties[6][0]+".png");
+        this.load.image("preKey", "./assets/"+properties[7][0]+".png");
+        this.load.image("postKey", "./assets/"+properties[7][1]+".png");
+        this.load.image("preLock", "./assets/"+properties[8][0]+".png");
+        this.load.image("postLock", "./assets/"+properties[8][1]+".png");
+        this.load.image("enemy", "./assets/"+properties[9][0]+".png");
 
+        this.mapTiles = [];
+
+        for(let row = 0; row < 10; ++row) {
+            let temp = [];
+            for(let col = 0; col < grid[0].length; ++col) {
+                //check and build
+            }
+            this.mapTiles.push(temp);
+        }
 
     }
 
--- src/prefabs/Tile.js
+++ src/prefabs/Tile.js
@@ -1,11 +1,43 @@
 class Tile extends Phaser.GameObjects.Sprite {
 
-    constructor(scene, x, y, texture, frame, type, character) {
+    constructor(scene, x, y, texture, frame, type, character, misc) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
 
-        
-
+        this.type = type;
+        this.character = character;
+        this.misc = misc;
     }
 
+    checkSpot(newChar) {
+        if(this.character != null) {
+            //fight time
+        }
+        if(this.character != null && this.character.currentHP <= 0) {
+            this.character = null;
+        }
+        if(this.type = "danger") {
+            //survival check
+        }
+        if(this.type = "special") {
+            //perform action
+        }
+        if(newChar.currentHP > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    moveTo(newChar) {
+        this.character = newChar;
+    }
+
+    moveFrom() {
+        this.character = null;
+    }
+
+    changeType(newType) {
+        this.type = newType;
+    }
 }
